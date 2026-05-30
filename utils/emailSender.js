c2a4ba9bc4c197b496bdda9b97a48d07212d9052
NoorAzam-1 <azamnoor698@gmail.com>
2026-05-30 16:33:32 +0530

    Add email sender utility

---
+++ utils/emailSender.js
@@ -0,0 +1,24 @@
+import nodemailer from "nodemailer";
+
+const transporter = nodemailer.createTransport({
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  auth: {
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS,
+  },
+});
+
+export const sendEmail = async (to, subject, html) => {
+  try {
+    const info = await transporter.sendMail({
+      from: '"Ebook Store" <no-reply@brainHub.com>',
+      to,
+      subject,
+      html,
+    });
+
+  } catch (error) {
+    console.error("Email error:", error);
+  }
+};
